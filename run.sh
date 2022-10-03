3c9a0dad26c8ad83168a7530548f849a5aa8e379
peekpoke <peekpoke@qq.com>
2022-10-04 07:03:58 +0800

    dnsmasq log and Rocky8/Ubuntu test

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-docker run -it -d --rm --name=pxe --network=host --privileged -v /data/tftp/OS:/var/lib/tftpboot/OS -v /data/tftp/ISO:/var/lib/tftpboot/ISO peekpoke/pxe 
+docker run -it -d --rm --name=pxe --network=host --privileged \
+    -v /data/tftp/OS:/var/lib/tftpboot/OS \
+    -v /data/tftp/ISO:/var/lib/tftpboot/ISO \
+    -v $(pwd)/images:/var/lib/tftpboot/images \
+    peekpoke/pxe 
